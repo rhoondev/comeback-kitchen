391b71afb38a4c247bcb8541e341c22f26001ee3
Rowan Faulkner <125912239+rhoondev@users.noreply.github.com>
2025-03-25 11:02:25 +0100

    Enabled lever control for faucet
    Temporarily disabled XR in settings and in scene because it was preventing me from entering play mode without the headset.

--- Comeback Kitchen/Assets/Scripts/Faucet.cs
+++ Comeback Kitchen/Assets/Scripts/Faucet.cs
@@ -3,26 +3,15 @@ using UnityEngine;
 public class Faucet : MonoBehaviour
 {
     [SerializeField] private ParticleSystem stream;
+    [SerializeField] private Transform lever;
+    [SerializeField] private float maxFlowRate;
 
     private float flowRate;
 
     // Update is called once per frame
     private void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            flowRate += Time.deltaTime;
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            flowRate -= Time.deltaTime;
-        }
-
-        if (flowRate < 0f)
-        {
-            flowRate = 0f;
-        }
+        flowRate = lever.localEulerAngles.z / 30f * maxFlowRate;
 
         var emission = stream.emission;
         emission.rateOverTimeMultiplier = flowRate;
