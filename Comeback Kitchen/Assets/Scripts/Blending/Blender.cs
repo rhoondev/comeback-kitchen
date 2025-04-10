fe9b0f1043741889f469d09c431d8938ad3e6d10
Rowan Faulkner <125912239+rhoondev@users.noreply.github.com>
2025-04-10 11:21:40 +0200

    Created blender script

---
+++ Comeback Kitchen/Assets/Scripts/Blending/Blender.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Blender : MonoBehaviour
+{
+    [SerializeField] private Rigidbody blade;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float acceleration;
+
+    private float _currentSpeed = 0f;
+    private bool _isOn = false;
+
+    private void Update()
+    {
+        if (Keyboard.current.bKey.wasPressedThisFrame)
+        {
+            _isOn = !_isOn;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isOn)
+        {
+            _currentSpeed = Mathf.MoveTowards(_currentSpeed, maxSpeed, acceleration * Time.fixedDeltaTime);
+        }
+        else
+        {
+            _currentSpeed = Mathf.MoveTowards(_currentSpeed, 0f, acceleration * Time.fixedDeltaTime);
+        }
+
+        blade.MoveRotation(Quaternion.Euler(0f, _currentSpeed * Time.fixedDeltaTime, 0f) * blade.rotation);
+    }
+}
