3a40a37ece038a4602beac2b0a78474c857d5890
Rowan Faulkner <125912239+rhoondev@users.noreply.github.com>
2025-05-05 14:32:13 +0200

    Fixed infinite grab spawner bug

--- Comeback Kitchen/Assets/Scripts/VR/DynamicGrabInteractable.cs
+++ Comeback Kitchen/Assets/Scripts/VR/DynamicGrabInteractable.cs
@@ -5,6 +5,8 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 [RequireComponent(typeof(Rigidbody))]
 public class DynamicGrabInteractable : XRGrabInteractable
 {
+    [field: SerializeField] public bool EnterDynamicModeOnGrabbed { get; set; } = true;
+
     private Rigidbody _rb;
 
     protected override void Awake()
@@ -16,11 +18,14 @@ public class DynamicGrabInteractable : XRGrabInteractable
     protected override void OnSelectEntering(SelectEnterEventArgs args)
     {
         // BEFORE grab is fully processed
-        _rb.interpolation = RigidbodyInterpolation.Interpolate;
-        _rb.isKinematic = false;
-        _rb.useGravity = true;
-        _rb.linearVelocity = Vector3.zero;
-        _rb.angularVelocity = Vector3.zero;
+        if (EnterDynamicModeOnGrabbed)
+        {
+            _rb.interpolation = RigidbodyInterpolation.Interpolate;
+            _rb.isKinematic = false;
+            _rb.useGravity = true;
+            _rb.linearVelocity = Vector3.zero;
+            _rb.angularVelocity = Vector3.zero;
+        }
 
         base.OnSelectEntering(args); // Make sure to call base
     }
