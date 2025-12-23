64da21d23726a5cf2172780d9ff4f5756154dc99
Rowan Faulkner <125912239+rhoondev@users.noreply.github.com>
2025-12-22 19:32:23 -0800

    Fixed faucet lever

--- Comeback Kitchen/Assets/Scripts/Washing/Faucet.cs
+++ Comeback Kitchen/Assets/Scripts/Washing/Faucet.cs
@@ -1,64 +1,89 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class Faucet : MonoBehaviour
 {
-    [SerializeField] private InteractionLocker leverInteractionLocker;
+    [SerializeField] private HingeJoint leverHingeJoint;
+    [SerializeField] private InteractionLocker leverHingeInteractionLocker;
+    [SerializeField] private XRGrabInteractable leverHingeInteractable;
     [SerializeField] private ParticleSystem stream;
-    [SerializeField] private Transform lever;
     [SerializeField] private float maxFlowRate;
 
     public SmartAction OnTurnedFullyOn = new SmartAction();
     public SmartAction OnTurnedFullyOff = new SmartAction();
 
+    private float _maxAngle;
     private float _flowRate;
     private bool _turnedFullyOn = false;
     private bool _turnedFullyOff = true;
 
+    private void Start()
+    {
+        _maxAngle = leverHingeJoint.limits.max;
+    }
+
+    private void OnEnable()
+    {
+        leverHingeInteractable.selectEntered.AddListener(OnHingeSelectEntering);
+        leverHingeInteractable.selectExited.AddListener(OnHingeSelectExiting);
+    }
+
+    private void OnDisable()
+    {
+        leverHingeInteractable.selectEntered.RemoveListener(OnHingeSelectEntering);
+        leverHingeInteractable.selectExited.RemoveListener(OnHingeSelectExiting);
+    }
+
     public void UnlockLever()
     {
-        leverInteractionLocker.UnlockInteraction();
+        leverHingeInteractionLocker.UnlockInteraction();
     }
 
     public void LockLever()
     {
-        leverInteractionLocker.LockInteraction();
+        leverHingeInteractionLocker.LockInteraction();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        float angle = lever.localEulerAngles.z;
+        float angle = leverHingeJoint.transform.localEulerAngles.z;
+        float eps = 0.1f;
 
-        if (angle > 180f)
-        {
-            angle -= 360f;
-        }
-
-        angle = Mathf.Clamp(angle, 0f, 30f);
-
-        lever.localEulerAngles = new Vector3(lever.localEulerAngles.x, lever.localEulerAngles.y, angle);
-
-        if (angle == 30f && !_turnedFullyOn)
+        if (angle >= _maxAngle - eps && !_turnedFullyOn)
         {
             _turnedFullyOn = true;
             _turnedFullyOff = false;
             OnTurnedFullyOn.Invoke();
         }
-        else if (angle == 0f && !_turnedFullyOff)
+        else if (angle <= eps && !_turnedFullyOff)
         {
             _turnedFullyOff = true;
             _turnedFullyOn = false;
             OnTurnedFullyOff.Invoke();
         }
-        else if (angle > 0f && angle < 30f)
+        else if (angle > eps && angle < _maxAngle - eps)
         {
             _turnedFullyOn = false;
             _turnedFullyOff = false;
         }
 
-        _flowRate = angle / 30f * maxFlowRate;
+        _flowRate = angle / _maxAngle * maxFlowRate;
 
         var emission = stream.emission;
         emission.rateOverTimeMultiplier = _flowRate;
+    }
+
+    private void OnHingeSelectEntering(SelectEnterEventArgs args)
+    {
+        // Disable damper
+        leverHingeJoint.useSpring = false;
+    }
+
+    private void OnHingeSelectExiting(SelectExitEventArgs args)
+    {
+        // Enable damper
+        leverHingeJoint.useSpring = true;
     }
 }
