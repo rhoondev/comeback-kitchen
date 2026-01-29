a8b609351cc983593f74b9e5d0502781f19dcd75
Rowan Faulkner <125912239+rhoondev@users.noreply.github.com>
2026-01-29 14:35:46 -0500

    Fixed seasoning

--- Comeback Kitchen/Assets/Scripts/Seasoning/SeasoningWeight.cs
+++ Comeback Kitchen/Assets/Scripts/Seasoning/SeasoningWeight.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class SeasoningWeight : MonoBehaviour
 {
+    [SerializeField] private Transform shaker;
+
     public event Action<Vector3, Vector3> OnCollisionWithTop;
     public event Action<Vector3> OnCollisionWithWall;
 
@@ -15,6 +17,16 @@ public class SeasoningWeight : MonoBehaviour
     {
         _rigidbody = GetComponent<Rigidbody>();
         transform.SetParent(null);
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, shaker.position) > 2f)
+        {
+            transform.position = shaker.position;
+            _rigidbody.linearVelocity = Vector3.zero;
+            _rigidbody.angularVelocity = Vector3.zero;
+        }
     }
 
     private void FixedUpdate()
